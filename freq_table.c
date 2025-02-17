d7e9e0432f11a16db0d0d5c721f8dadd3955d857
Bran Quanne <branmq@gmail.com>
2025-02-17 12:13:47 +0100

    testggs

--- freq_table.c
+++ freq_table.c
@@ -3,15 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-
 int main(void)
 {
 	int table[256];
- //testing testing atention please
- //INTE EMINEM
-	
+	// testing testing atention please
+	// INTE EMINEM eller kanye
 
 	return 0;
 }

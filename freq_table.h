dc953d5f12c18a39ec1f1e87890515749c861d54
Henrikroj <henrik.rojvall@gmail.com>
2025-02-23 17:19:16 +0100

    hej

--- freq_table.h
+++ freq_table.h
@@ -1,6 +1,29 @@
+#ifndef FREQ_TABLE_H
+#define FREQ_TABLE_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#pragma once
+/**
+ * @defgroup freq_table_h Frequency Table
+ * 
+ * @brief A module for creating a frequency table.
+ * 
+ * This module provides a function for creating a frequency table
+ * from a string of characters. The frequency table is an array
+ * of integers where each index represents a character and the 
+ * value at that index represents the frequency of that character.
+ * 
+ * @author  Henrik Röjvall & Bran Quanne
+ * @since   2025-02-23
+ * 
+ * @{
+ */
 
 int *checkFrequency(char *fileContents);
+
+#endif /* FREQ_TABLE_H */
+
+/**
+ * @}
+ */

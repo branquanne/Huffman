c6feb7e36d5cf934f63401faf0de80187cab39fc
Bran Quanne <branmq@gmail.com>
2025-02-23 19:39:01 +0100

    la till .clang-format

--- validate_data.c
+++ validate_data.c
@@ -8,10 +8,10 @@
 /*
  * File: validate_data.c
  *
- * Description: 
- *               
- *              
- *              
+ * Description:
+ *
+ *
+ *
  *
  * Author: Henrik Röjvall & Bran Quanne
  *
@@ -22,42 +22,41 @@
 /*  External functions  */
 
 void checkNumberOfArguments(int numberOfArguments) {
-    if (numberOfArguments != 5) {
-        printf("Usage: %s\n [OPTION] [FILE0] [FILE1] [FILE2]\n", "huffman");
-        printf("Options:\n");
-        printf("-encode encodes FILE1 according to frequence analysis done on FILE0. Stores the result in FILE2\n");
-        printf("-decode decodes FILE1 according to frequence analysis done on FILE0.Stores the result in FILE2\n");
-        exit(1);
-    }
+  if (numberOfArguments != 5) {
+    printf("Usage: %s\n [OPTION] [FILE0] [FILE1] [FILE2]\n", "huffman");
+    printf("Options:\n");
+    printf("-encode encodes FILE1 according to frequence analysis done on FILE0. Stores the result in FILE2\n");
+    printf("-decode decodes FILE1 according to frequence analysis done on FILE0.Stores the result in FILE2\n");
+  }
 }
 
 void checkOptionValidity(char **arguments) {
-    if (strcmp(arguments[1], "-encode") != 0 && strcmp(arguments[1], "-decode") != 0) {
-        printf("Invalid option: %s\n", arguments[1]);
-        exit(1);
-    }
+  if (strcmp(arguments[1], "-encode") != 0 && strcmp(arguments[1], "-decode") != 0) {
+    printf("Invalid option: %s\n", arguments[1]);
+    exit(1);
+  }
 }
 
 char *loadFileCharacters(char *fileName) {
-    FILE *file = fopen(fileName, "rb");
-    if (file == NULL) {
-        printf("Could not open file %s\n", fileName);
-        exit(1);
-    }
+  FILE *file = fopen(fileName, "rb");
+  if (file == NULL) {
+    printf("Could not open file %s\n", fileName);
+    exit(1);
+  }
 
-    fseek(file, 0, SEEK_END);
-    long fileSize = ftell(file);
-    fseek(file, 0, SEEK_SET);
+  fseek(file, 0, SEEK_END);
+  long fileSize = ftell(file);
+  fseek(file, 0, SEEK_SET);
 
-    char *fileContent = malloc(fileSize + 1);
-    if (fileContent == NULL) {
-        printf("Could not allocate memory for file content\n");
-        exit(1);
-    }
+  char *fileContent = malloc(fileSize + 1);
+  if (fileContent == NULL) {
+    printf("Could not allocate memory for file content\n");
+    exit(1);
+  }
 
-    fread(fileContent, 1, fileSize, file);
-    fileContent[fileSize] = '\0';
-    fclose(file);
+  fread(fileContent, 1, fileSize, file);
+  fileContent[fileSize] = '\0';
+  fclose(file);
 
-    return fileContent;
+  return fileContent;
 }

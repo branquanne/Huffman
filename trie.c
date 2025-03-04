11e0531c08ef7536a0f97fb4a69fc91b9af99a2d
Bran Quanne <branmq@gmail.com>
2025-03-04 13:28:04 +0100

    dnadiadiaidjaidja

--- trie.c
+++ trie.c
@@ -5,8 +5,12 @@
 
 // Comparator using pqueue.h pqueue_cmp_func
 int compareValues(void *val1, void *val2) {
+
+  // Cast the void pointers to TrieNode pointers
   TrieNode *node1 = (TrieNode *)val1;
   TrieNode *node2 = (TrieNode *)val2;
+
+  // Return the difference between the frequencies
   return node1->frequency - node2->frequency;
 }
 
@@ -27,6 +31,8 @@ TrieNode *buildHuffmanTrie(int *frequencyTable) {
   // Create leaf nodes and insert them into the priority queue
   for (int i = 0; i < 256; i++) {
     if (frequencyTable[i] >= 0) {
+
+      // Create a new node with the index as the character and the frequency from the frequency table
       TrieNode *node = createNode((unsigned char)i, frequencyTable[i]);
       pqueue_insert(pq, node);
     }
@@ -34,8 +40,12 @@ TrieNode *buildHuffmanTrie(int *frequencyTable) {
 
   //  Build the Huffman Trie
   while (pqueue_is_empty(pq) == false) {
+
+    // If there is only one node left in the priority queue, break
     TrieNode *left = (TrieNode *)pqueue_inspect_first(pq);
     pqueue_delete_first(pq);
+
+    // If the priority queue is empty, insert the last node
     if (pqueue_is_empty(pq)) {
       pqueue_insert(pq, left);
       break;
@@ -43,10 +53,12 @@ TrieNode *buildHuffmanTrie(int *frequencyTable) {
     TrieNode *right = (TrieNode *)pqueue_inspect_first(pq);
     pqueue_delete_first(pq);
 
+    // Create an internal node
     TrieNode *internalNode = createNode('\0', left->frequency + right->frequency);
     internalNode->left = left;
     internalNode->right = right;
 
+    // Insert the internal node into the priority queue
     pqueue_insert(pq, internalNode);
   }
 

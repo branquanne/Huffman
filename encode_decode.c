66bf359a1d2c11f539fef31fc2add60ed28c98de
Henrikroj <henrik.rojvall@gmail.com>
2025-03-06 10:58:36 +0100

    hej

--- encode_decode.c
+++ encode_decode.c
@@ -3,20 +3,13 @@
 #include "trie.h"
 
 // Function to encode a file
-void endodeFile(char *fileToEncode, char *outFile, HuffmanTable *table) {
+void encodeFile(char *fileToEncode, char *outFile, HuffmanTable *table) {
 
-  // freq_table
-  int *freq_table = checkFrequency(fileToEncode);
+  // Open the input file
+  FILE *in = fopen(fileToEncode, "rb");
 
   // Open the output file
   FILE *out = fopen(outFile, "wb");
-  if (out == NULL) {
-    printf("Could not open file %s\n", outFile);
-    fclose(in);
-    return;
-  }
-
-  
 
   // Create a bit buffer
   bit_buffer *buffer = bit_buffer_empty();
@@ -24,23 +17,25 @@ void endodeFile(char *fileToEncode, char *outFile, HuffmanTable *table) {
   // Read the input file character by character
   int c;
   while ((c = fgetc(in)) != EOF) {
-
-    // Find the character in the Huffman Table
-    for (size_t i = 0; i < table->size; i++) {
-      if (table->entries[i].character == (unsigned char)c) {
-
-        // Append the bit sequence to the buffer
-        bit_buffer_append(buffer, table->entries[i].bit_sequence);
+   
+    for (size_t i = 0; i table->size; i++) {
+      if (table->entries[i].character == c) {
+        for (size_t j = 0 < bit_buffer_size(table->entries[i].bit_sequence); j++) {
+          bit_buffer_append_bit(buffer, bit_buffer_get_bit(table->entries[i].bit_sequence, j));
+        }
         break;
       }
-    }
+    } 
   }
 
-  // Write the buffer to the output file
-  bit_buffer_write(buffer, out);
+  char *byteArray = bit_buffer_to_byte_array(buffer);
+  fwrite(byteArray, sizeof(char), bit_buffer_size(buffer) / 8, out);
 
-  // Free the bit buffer
-  bit_buffer_free(buffer);
+  // Free the byte array
+  free(byteArray);
+
+  // Clear the bit buffer
+  bit_buffer_clear(buffer); 
 
   // Close the files
   fclose(in);

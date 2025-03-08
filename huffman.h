c9d0ea762f18ef88f0d488124c35a97bb3eadc3b
Henrikroj <henrik.rojvall@gmail.com>
2025-03-08 13:27:50 +0100

    sui

--- huffman.h
+++ huffman.h
@@ -17,6 +17,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * @defgroup huffman_h Huffman
+ * 
+ * @brief A module for implementing Huffman coding for file compression
+ * and decompression.
+ * 
+ * This module provides a compare function for nodes of a Huffman tree.abort
+ * 
+ * @author Henrik Röjvall & Bran Quanne
+ * @since 2025-03-08
+ * 
+ * @{
+ */
+
+
+/**
+ * @brief Compares two huffman tree nodes based on their frequency.
+ * 
+ * Used as a comparator function for the priority queue.
+ * 
+ * @param a A pointer to the first node.
+ * @param b A pointer to the second node.
+ * 
+ * @return A negative value if a has lower frequency than b, 
+ * a positive value if a has a higher frequenxy, and zero if they are equal.
+ */
 int node_cmp(void *a, void *b);
 
 #endif // HUFFMAN_H
+
+/**
+ * @}
+ */

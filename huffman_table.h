fbac400368dd559dfe0f4fdee2a6deeeb81a3b50
Henrikroj <henrik.rojvall@gmail.com>
2025-03-08 13:27:30 +0100

    sui

--- huffman_table.h
+++ huffman_table.h
@@ -3,8 +3,36 @@
 
 #include "trie.h"
 
+/**
+ * @defgroup huffman_table_h Huffman
+ * 
+ * @brief A module for generationg Huffman codes from a Huffman tree.abort
+ * 
+ * This module provides a function to generate binary Huffman codes
+ * for characters based on their frequency.
+ * 
+ * @author Henrik Röjvall & Bran Quanne
+ * @since 2025-03-08
+ * 
+ * @{
+ */
 
-// Function to generate Huffman codes
+/**
+ * @brief Generates Huffman codes for characters in the Huffman tree.
+ * 
+ * Traverses the Huffman tree recursively to assign binary codes to
+ * each character and stores them in an array.
+ * 
+ * @param root The root of the Huffman tree.
+ * @param code Array used to store the current Huffman code during traversal.
+ * @param top Index representing the current position in the code array.
+ * @param codes Array where the generated Huffman codes will be stored.
+ * 
+ */
 void generate_huffman_codes(Node *root, char *code, int top, char *codes[]);
 
 #endif // HUFFMAN_TABLE_H
+
+/** 
+ * @} 
+ */
--- trie.h
+++ trie.h
@@ -4,17 +4,62 @@
 #include "freq_table.h"
 #include "pqueue.h"
 
-// Define the Node structure
+/**
+ * @defgroup trie_h Huffman
+ * 
+ * @brief A module for building a Huffman tree using a priority queue.
+ * 
+ * This module provides functions to create a new node and build a Huffman
+ * tree based on character frequency.
+ * 
+ * @author Henrik Röjvall & Bran Quanne
+ * @since 2025-03-08
+ * 
+ * @{
+ */
+
+/**
+ * @brief A structure to represent a node in the Huffman tree.
+ * 
+ * Each node stores a character, its frequency, and pointers to left and
+ * right child nodes.
+ */
 typedef struct Node {
-	char data;
-	unsigned freq;
-	struct Node *left, *right;
+	char data;					/**< The character stored in the node */
+	unsigned freq;				/**< The frequency of the character */
+	struct Node *left, *right;  /**< Pointers to the left and right child nodes */
   } Node;
 
-// Function to create a new node
+/**
+ * @brief Creates a new Huffman tree node.
+ * 
+ * Allocates memory for a new node and initializes its data and frequency.
+ * 
+ * @param data The character to store in the node.
+ * @param freq The frequency of the character.
+ * 
+ * @return A pointer to the newly created node.
+ * 
+ * @note The caller is responsible for managing the allocated memory.
+ */
 Node *create_node(char data, unsigned freq);
 
-// Function to build the Huffman tree
+/**
+ * @brief Builds a Huffman tree from character frequencies.
+ *
+ * Constructs a Huffman tree using a priority queue where nodes with the lowest 
+ * frequency have the highest priority.
+ *
+ * @param freq An array representing the frequency of each ASCII character.
+ *
+ * @return A pointer to the root node of the Huffman tree.
+ *
+ * @note The caller is responsible for freeing the allocated memory when done.
+ */
 Node *build_huffman_tree(int *freq);
 
 #endif // TRIE_H
+
+/**
+ * @}
+ */
